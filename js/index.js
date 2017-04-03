6016137fbfbcd8d68e91f4cd334d48995f28aa8b
YareArista <yarearista@gmail.com>
2017-04-02 20:17:10 -0500

    modals

---
+++ js/index.js
@@ -0,0 +1,9 @@
+  function metodologia() {
+    document.getElementById('metodologia').classList.toggle("hidden-pop");
+  }
+  function clasificacion() {
+    document.getElementById('clasificacion').classList.toggle("hidden-pop");
+  }
+  function que() {
+    document.getElementById('que').classList.toggle("hidden-pop");
+  }
