ef6a4df0ec015f146c1fc3226f6aae16c04d1f0d
YareArista <yarearista@gmail.com>
2017-04-07 20:35:54 -0500

    funcionalidad

--- js/index.js
+++ js/index.js
@@ -7,3 +7,59 @@
   function que() {
     document.getElementById('que').classList.toggle("hidden-pop");
   }
+
+
+// funcionamiento del menu
+
+
+function efectividad() {
+    document.getElementById('efectividad').classList.toggle("hidden-pop");
+  }
+  function efectividadUno() {
+    document.getElementById('efectividadUno').classList.toggle("hidden-pop");
+  }
+  function efectividadDos() {
+    document.getElementById('efectividadDos').classList.toggle("hidden-pop");
+  }
+  function efectividadTres() {
+    document.getElementById('efectividadTres').classList.toggle("hidden-pop");
+  }
+
+function prevencion() {
+    document.getElementById('prevencion').classList.toggle("hidden-pop");
+  }
+  function prevencionUno() {
+    document.getElementById('prevencionUno').classList.toggle("hidden-pop");
+  }
+  function prevencionDos() {
+    document.getElementById('prevencionDos').classList.toggle("hidden-pop");
+  }
+  function prevencionTres() {
+    document.getElementById('prevencionTres').classList.toggle("hidden-pop");
+  }
+
+function intervencion() {
+    document.getElementById('intervencion').classList.toggle("hidden-pop");
+  }
+  function intervencionUno() {
+    document.getElementById('intervencionUno').classList.toggle("hidden-pop");
+  }
+  function intervencionDos() {
+    document.getElementById('intervencionDos').classList.toggle("hidden-pop");
+  }
+  function intervencionTres() {
+    document.getElementById('ntervencionTres').classList.toggle("hidden-pop");
+  }
+
+function poblacion() {
+    document.getElementById('poblacion').classList.toggle("hidden-pop");
+  }
+  function poblacionUno() {
+    document.getElementById('poblacionUno').classList.toggle("hidden-pop");
+  }
+  function poblacionDos() {
+    document.getElementById('poblacionDos').classList.toggle("hidden-pop");
+  }
+  function poblacionTres() {
+    document.getElementById('poblacionTres').classList.toggle("hidden-pop");
+  }
