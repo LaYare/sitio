ca2e556e9d1c7f0f8c0df48e88c5880ba442bb45
YareArista <yarearista@gmail.com>
2017-04-08 03:05:31 -0500

    pagina terminada

--- js/index.js
+++ js/index.js
@@ -15,54 +15,156 @@
 
 function efectividad() {
     document.getElementById('efectividad').classList.toggle("hidden-pop");
+     var icono = document.getElementById("iconoUno");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function efectividadUno() {
     document.getElementById('efectividadUno').classList.toggle("hidden-pop");
+     var icono = document.getElementById("iconoDos");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function efectividadDos() {
     document.getElementById('efectividadDos').classList.toggle("hidden-pop");
+     var icono = document.getElementById("iconoTres");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function efectividadTres() {
     document.getElementById('efectividadTres').classList.toggle("hidden-pop");
+     var icono = document.getElementById("iconoCuatro");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
+
 
 function prevencion() {
     document.getElementById('prevencion').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoCinco");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function prevencionUno() {
     document.getElementById('prevencionUno').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoSeis");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function prevencionDos() {
     document.getElementById('prevencionDos').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoSiete");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    };
+    document.getElementById('desarrollo').classList.toggle("hidden-pop");
+
   }
   function prevencionTres() {
     document.getElementById('prevencionTres').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoOcho");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
 
 function intervencion() {
     document.getElementById('intervencion').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoNueve");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function intervencionUno() {
     document.getElementById('intervencionUno').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoDiez");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    };
+    document.getElementById('programas').classList.toggle("hidden-pop");
+
   }
   function intervencionDos() {
     document.getElementById('intervencionDos').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoOnce");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function intervencionTres() {
-    document.getElementById('ntervencionTres').classList.toggle("hidden-pop");
+    document.getElementById('intervencionTres').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoDoce");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
 
 function poblacion() {
     document.getElementById('poblacion').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoTrece");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function poblacionUno() {
     document.getElementById('poblacionUno').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoCatorce");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function poblacionDos() {
     document.getElementById('poblacionDos').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoQuince");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    }
   }
   function poblacionTres() {
     document.getElementById('poblacionTres').classList.toggle("hidden-pop");
+    var icono = document.getElementById("iconoDiezSeis");
+    if (icono.className == "fa fa-plus") {
+      icono.className = "fa fa-window-minimize";
+    }else {
+      icono.className = "fa fa-plus";
+    };
+    document.getElementById('terciaria').classList.toggle("hidden-pop");
   }
 /*menu de abajo*/
 
@@ -97,5 +199,85 @@ function poblacion() {
   function textEight() {
     document.getElementById('textEight').classList.toggle("hidden-pop");
   }
+
+  function circulos() {
+    document.getElementById('circulos-de-apoyo').classList.toggle("hidden-pop");
+  }
+   function juvenil() {
+    document.getElementById('juvenil').classList.toggle("hidden-pop");
+  }
+   function focalizada() {
+    document.getElementById('focalizada').classList.toggle("hidden-pop");
+  }
+/*Estrategias de difusión focalizada*/
+
+  function textt() {
+    document.getElementById('textt').classList.toggle("hidden-pop");
+  }
+
+  function textTwoo() {
+    document.getElementById('textTwoo').classList.toggle("hidden-pop");
+  }
+
+  function textThreee() {
+    document.getElementById('textThreee').classList.toggle("hidden-pop");
+  }
+
+  function textFourr() {
+    document.getElementById('textFourr').classList.toggle("hidden-pop");
+  }
+
+  function textFivee() {
+    document.getElementById('textFivee').classList.toggle("hidden-pop");
+  }
+
+  function textSixx() {
+    document.getElementById('textSixx').classList.toggle("hidden-pop");
+  }
+
+  function textSevenn() {
+    document.getElementById('textSevenn').classList.toggle("hidden-pop");
+  }
+
+  function textEightt() {
+    document.getElementById('textEightt').classList.toggle("hidden-pop");
+  }
+
+
+/*Programas de concientización juvenil*/
+
+  function texttt() {
+    document.getElementById('texttt').classList.toggle("hidden-pop");
+  }
+
+  function textTwooo() {
+    document.getElementById('textTwooo').classList.toggle("hidden-pop");
+  }
+
+  function textThreeee() {
+    document.getElementById('textThreeee').classList.toggle("hidden-pop");
+  }
+
+  function textFourrr() {
+    document.getElementById('textFourrr').classList.toggle("hidden-pop");
+  }
+
+  function textFiveee() {
+    document.getElementById('textFiveee').classList.toggle("hidden-pop");
+  }
+
+  function textSixxx() {
+    document.getElementById('textSixxx').classList.toggle("hidden-pop");
+  }
+
+  function textSevennn() {
+    document.getElementById('textSevennn').classList.toggle("hidden-pop");
+  }
+
+  function textEighttt() {
+    document.getElementById('textEighttt').classList.toggle("hidden-pop");
+  }
+
+
 
 
